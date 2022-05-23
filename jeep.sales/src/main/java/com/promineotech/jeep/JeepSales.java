911d6703244e7a2bff8e892d05d7f91ef27e8de1
Chelsea Perrine <perrinechelsea@gmail.com>
2022-05-23 14:51:09 -0500

    May23 Second commit

--- jeep.sales/src/main/java/com/promineotech/jeep/JeepSales.java
+++ jeep.sales/src/main/java/com/promineotech/jeep/JeepSales.java
@@ -3,7 +3,10 @@ package com.promineotech.jeep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.promineotech.ComponentScanMarker;
+
+@SpringBootApplication(scanBasePackageClasses = { ComponentScanMarker.class})
+
 public class JeepSales {
 
 	public static void main(String[] args) {
--- jeep.sales/src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
+++ jeep.sales/src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
@@ -15,7 +15,7 @@ public class BasicJeepSalesController implements JeepSalesController {
 
 	@Override
 	public List<Jeep> fetchJeeps(String model, String trim) {
-		log.info("model={}, trim{}", model, trim);
+		log.debug("model={}, trim{}", model, trim);
 		return null;
 	}
 
