d78213a03704259ec4e13c35481edee37c2b3f47
Chelsea Perrine <perrinechelsea@gmail.com>
2022-05-24 08:48:37 -0500

    may24 push

--- jeep.sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ jeep.sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.http.HttpMethod;
 
 import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
@@ -17,7 +18,9 @@ import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class FetchJeepTest extends FetchJeepTestSupport {
+
 
 	@Test
 	void testThatJeepsAreReturnedWhenAValidModelAndTrumAreSupplied() {
@@ -30,11 +33,20 @@ class FetchJeepTest extends FetchJeepTestSupport {
 
 		// When: a connection is made to the URI
 		ResponseEntity<List<Jeep>> response = getRestTemplate().exchange(uri, HttpMethod.GET, null,
-				new ParameterizedTypeReference<>() {
-				});
+				new ParameterizedTypeReference<>() {});
 
 		// Then: a success (OK 200) is returned
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		//And: the actual list returned is the same as the expected list
+		List<Jeep> expected = buildExpected();
+		System.out.println(expected);
+		assertThat(response.getBody()).isEqualTo(expected);
+		
+	}
+
+	protected List<Jeep> buildExpected() {
+		return null;
 	}
 
 }
